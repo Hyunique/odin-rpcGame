0753487c41a48dd298946f953b0c966d1de8c935
Elena <kunstvanelena@gmail.com>
2022-06-08 15:41:53 +0200

    report a winner or loser at the end of the game

--- index.js
+++ index.js
@@ -1,51 +1,18 @@
+/* pick rock paper or scissors randomly */
 function computerPlay() {
     let rps = ['rock', 'paper', 'scissors']
     const random = Math.floor(Math.random() * rps.length);
     return rps[random];
 }
 
-let myScore = 0;
-let compScore = 0;
+let score = {
+    'myScore': 0,
+    'compScore': 0
+};
 
 
-// function playRound(playerSelection, computerSelection) {
-
-//     playerSelection = prompt("Choose your method!").toLowerCase();
-//     computerSelection = computerPlay();
-
-
-//     if (playerSelection === 'rock') {
-//         if (computerSelection === 'paper') {
-//             compScore += 1
-//             return "You lose! Paper beats Rock"
-//         } else if (computerSelection === 'scissors') {
-//             myScore += 1
-//             return "You win! Rock beats Scissors"
-//         }
-//     } else if (playerSelection === 'paper') {
-//         if (computerSelection === 'rock') {
-//             myScore += 1
-//             return "You win! Paper beats Rock!"
-//         } else if (computerSelection === 'scissors') {
-//             compScore += 1
-//             return "You lose! Scissors beats Paper"
-//         }
-//     } else if (playerSelection === 'scissors') {
-//         if (computerSelection === 'rock') {
-//             compScore += 1
-//             return "You lose! Rock beats Scissors"
-//         } else if (computerSelection = 'paper') {
-//             myScore += 1
-//             return "You win! Scissors beats Paper"
-//         }
-//     } else { /* this should be the first line in the function*/
-//         return "Tie game!"
-//     }
-
-// }
-
-
-
+/* plays a single round of Rock Paper Scissors 
+and declares the winner of the round */
 function playRound(playerSelection, computerSelection) {
     playerSelection = prompt("Choose your method!").toLowerCase();
     computerSelection = computerPlay();
@@ -57,39 +24,49 @@ function playRound(playerSelection, computerSelection) {
         return "Tie game!"
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            compScore += 1
+            score.compScore += 1
             return "You lose! Paper beats Rock"
         } else if (computerSelection === 'scissors') {
-            myScore += 1
+            score.myScore += 1
             return "You win! Rock beats Scissors"
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
-            myScore += 1
+            score.myScore += 1
             return "You win! Paper beats Rock!"
         } else if (computerSelection === 'scissors') {
-            compScore += 1
+            score.compScore += 1
             return "You lose! Scissors beats Paper"
         }
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
-            compScore += 1
+            score.compScore += 1
             return "You lose! Rock beats Scissors"
         } else if (computerSelection = 'paper') {
-            myScore += 1
+            score.myScore += 1
             return "You win! Scissors beats Paper"
         }
     }
+
+
 }
 
+/*Compare both scores and announce winner*/
+function winner() {
+    if (score['myScore'] > score['compScore']) {
+        alert("You are the winner!")
+    } else if (score['myScore'] < score['compScore']) {
+        alert("You lost!")
+    } else {
+        alert("Draw! Same scores")
+    }
+}
 
-
-/* make a function called game 
-play a 5 round game that keeps score
- and reports a winner or loser at the end.*/
+/* Play a 5 round game and reports a winner or loser at the end.*/
 function game() {
     for (let i = 0; i < 5; i++) {
         console.log(playRound())
     }
+    winner()
 }
 
