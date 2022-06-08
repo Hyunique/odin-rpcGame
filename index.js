7e8e19eade39832784b548fc98f64b56d3e7377c
Elena <kunstvanelena@gmail.com>
2022-06-08 11:52:15 +0200

    git commit -m

--- index.js
+++ index.js
@@ -1,45 +1,94 @@
-// Play against the computer
-
-// function computerPlay returns R P or S.
-
-// Single round of RPS
-// function takes playerSelection and computerSelection
-// it returns a string that declares the winner
-
-
 function computerPlay() {
-    let rps = ['Rock', 'Paper', 'Scissors']
+    let rps = ['rock', 'paper', 'scissors']
     const random = Math.floor(Math.random() * rps.length);
     return rps[random];
 }
 
-/*make it case incensitive*/
-function playRound(playerSelection, computerSelection) {
+let myScore = 0;
+let compScore = 0;
 
-    if (playerSelection === 'Rock') {
-        if (computerSelection === 'Paper') {
+
+// function playRound(playerSelection, computerSelection) {
+
+//     playerSelection = prompt("Choose your method!").toLowerCase();
+//     computerSelection = computerPlay();
+
+
+//     if (playerSelection === 'rock') {
+//         if (computerSelection === 'paper') {
+//             compScore += 1
+//             return "You lose! Paper beats Rock"
+//         } else if (computerSelection === 'scissors') {
+//             myScore += 1
+//             return "You win! Rock beats Scissors"
+//         }
+//     } else if (playerSelection === 'paper') {
+//         if (computerSelection === 'rock') {
+//             myScore += 1
+//             return "You win! Paper beats Rock!"
+//         } else if (computerSelection === 'scissors') {
+//             compScore += 1
+//             return "You lose! Scissors beats Paper"
+//         }
+//     } else if (playerSelection === 'scissors') {
+//         if (computerSelection === 'rock') {
+//             compScore += 1
+//             return "You lose! Rock beats Scissors"
+//         } else if (computerSelection = 'paper') {
+//             myScore += 1
+//             return "You win! Scissors beats Paper"
+//         }
+//     } else { /* this should be the first line in the function*/
+//         return "Tie game!"
+//     }
+
+// }
+
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Choose your method!").toLowerCase();
+    computerSelection = computerPlay();
+
+    if (playerSelection === computerSelection) {
+        return "Tie game!"
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            compScore += 1
             return "You lose! Paper beats Rock"
         } else if (computerSelection === 'scissors') {
-            return "You win! Rock beats scissors"
+            myScore += 1
+            return "You win! Rock beats Scissors"
         }
-    } else if (playerSelection === 'Paper') {
-        if (computerSelection === 'Rock') {
-            return "You win! Paper beats rock!"
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            myScore += 1
+            return "You win! Paper beats Rock!"
         } else if (computerSelection === 'scissors') {
+            compScore += 1
             return "You lose! Scissors beats Paper"
         }
-    } else if (playerSelection === 'Scissors') {
-        if (computerSelection === 'Rock') {
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            compScore += 1
             return "You lose! Rock beats Scissors"
-        } else if (computerSelection = 'Paper') {
+        } else if (computerSelection = 'paper') {
+            myScore += 1
             return "You win! Scissors beats Paper"
         }
     } else {
-        return "Tie game!"
+        playerSelection = prompt("Give valid option!!!!").toLowerCase();
     }
 }
 
 
-const playerSelection = prompt("Choose your method!");
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+/* make a function called game 
+play a 5 round game that keeps score
+ and reports a winner or loser at the end.*/
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound())
+    }
+}
+
