56e9f536ff5a5acae63fd787d948dd6710c9ba48
Elena <kunstvanelena@gmail.com>
2022-07-01 12:09:21 +0200

    click event added on tool buttons

--- index.js
+++ index.js
@@ -8,7 +8,40 @@ startBtn.addEventListener('click', () => {
     introContainer.style.display = 'none';
 })
 
+let myScoreDisplay = document.querySelector('.myScoreDisplay')
+let compScoreDisplay = document.querySelector('.compScoreDisplay')
+let statusText = document.querySelector('.status')
+let myScore = 0
+let compScore = 0
 
+
+
+/*Get alt for playerselection and run playRound function*/
+let playerRock = document.querySelector('.rock')
+const rockAlt = playerRock.getAttribute('alt')
+playerRock.addEventListener('click', () => {
+    playerSelection = rockAlt;
+    computerSelection = computerPlay()
+    console.log(playerSelection, computerSelection)
+    playRound(playerSelection, computerSelection)
+})
+
+let playerPaper = document.querySelector('.paper')
+const paperAlt = playerPaper.getAttribute('alt')
+playerPaper.addEventListener('click', () => {
+    playerSelection = paperAlt;
+    computerSelection = computerPlay()
+    console.log(playerSelection, computerSelection)
+    playRound(playerSelection, computerSelection)
+})
+
+let playerScissors = document.querySelector('.scissors')
+const scissorsAlt = playerScissors.getAttribute('alt')
+playerScissors.addEventListener('click', () => {
+    playerSelection = scissorsAlt;
+    computerSelection = computerPlay()
+    playRound(playerSelection, computerSelection)
+})
 
 
 
@@ -17,71 +50,65 @@ startBtn.addEventListener('click', () => {
 function computerPlay() {
     let rps = ['rock', 'paper', 'scissors']
     const random = Math.floor(Math.random() * rps.length);
-    return rps[random];
+    return rps[random]
 }
-
-let score = {
-    'myScore': 0,
-    'compScore': 0
-};
 
 
 /* plays a single round of Rock Paper Scissors 
 and declares the winner of the round */
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Choose your method!").toLowerCase();
-    computerSelection = computerPlay();
-
-    while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-        playerSelection = prompt("Not a valid answer. Try again!").toLowerCase();
-    }
+    // while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+    //     playerSelection = prompt("Not a valid answer. Try again!").toLowerCase();
+    // }
     if (playerSelection === computerSelection) {
-        return "Tie game!"
+        statusText.textContent = "Tie game!"
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            score.compScore += 1
-            return "You lose! Paper beats Rock"
+            compScore += 1
+            statusText.textContent = "You lose! Paper beats Rock"
         } else if (computerSelection === 'scissors') {
-            score.myScore += 1
-            return "You win! Rock beats Scissors"
+            myScore += 1
+            statusText.textContent = "You win! Rock beats Scissors"
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
-            score.myScore += 1
-            return "You win! Paper beats Rock!"
+            myScore += 1
+            statusText.textContent = "You win! Paper beats Rock!"
         } else if (computerSelection === 'scissors') {
-            score.compScore += 1
-            return "You lose! Scissors beats Paper"
+            compScore += 1
+            statusText.textContent = "You lose! Scissors beats Paper"
         }
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
-            score.compScore += 1
-            return "You lose! Rock beats Scissors"
+            compScore += 1
+            statusText.textContent = "You lose! Rock beats Scissors"
         } else if (computerSelection = 'paper') {
-            score.myScore += 1
-            return "You win! Scissors beats Paper"
+            myScore += 1
+            statusText.textContent = "You win! Scissors beats Paper"
         }
     }
-
+    myScoreDisplay.textContent = myScore
+    compScoreDisplay.textContent = compScore
 
 }
+
 
 /*Compare both scores and announce winner*/
-function winner() {
-    if (score['myScore'] > score['compScore']) {
-        alert("You are the winner!")
-    } else if (score['myScore'] < score['compScore']) {
-        alert("You lost!")
-    } else {
-        alert("Draw! Same scores")
-    }
-}
+// function winner() {
+//     if (score['myScore'] > score['compScore']) {
+//         alert("You are the winner!")
+//     } else if (score['myScore'] < score['compScore']) {
+//         alert("You lost!")
+//     } else {
+//         alert("Draw! Same scores")
+//     }
+// }
 
 /* Play a 5 round game and reports a winner or loser at the end.*/
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound())
-    }
-    winner()
-}
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(playRound())
+//     }
+//     winner()
+// }
 
