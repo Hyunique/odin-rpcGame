71f86ab0bbd7bab42935a66c13f66099b2488844
Elena <kunstvanelena@gmail.com>
2022-07-05 17:01:22 +0200

    modified isWinner() to reveal end div

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let round = 0
 let winningScore = 3
 let toolBtn = document.querySelector('.toolBtn')
 
+
 /* Hide intro page and transit into play page */
 let startBtn = document.querySelector('.startButton')
 let introContainer = document.querySelector('.introContainer')
@@ -15,6 +16,7 @@ startBtn.addEventListener('click', () => {
     introContainer.style.transform = 'scale(0)';
     introContainer.style.display = 'none';
 })
+
 
 const gameNumber = document.querySelector('#gamenumber')
 gameNumber.addEventListener('change', function () {
@@ -30,7 +32,7 @@ playerRock.addEventListener('click', () => {
     computerSelection = computerPlay()
     // console.log(playerSelection, computerSelection)
     playRound(playerSelection, computerSelection)
-    isWinner()
+    if (round === winningScore) { setTimeout(isWinner, 500) }
 })
 
 let playerPaper = document.querySelector('.paper')
@@ -40,7 +42,7 @@ playerPaper.addEventListener('click', () => {
     computerSelection = computerPlay()
     // console.log(playerSelection, computerSelection)
     playRound(playerSelection, computerSelection)
-    isWinner()
+    if (round === winningScore) { setTimeout(isWinner, 500) }
 })
 
 let playerScissors = document.querySelector('.scissors')
@@ -49,18 +51,19 @@ playerScissors.addEventListener('click', () => {
     playerSelection = scissorsAlt;
     computerSelection = computerPlay()
     playRound(playerSelection, computerSelection)
-    isWinner()
+    if (round === winningScore) { setTimeout(isWinner, 500) }
 })
 
-let magicScore = document.querySelector('.magic')
-magicScore.addEventListener('click', () => {
-    let magicAnswer = prompt('Guess the magic keyword. You win if answer is correct!')
-    if (magicAnswer === 'fire') {
-        round += 1
-        myScore += 8
-        myScoreDisplay.textContent = myScore
-    }
-})
+// let magicScore = document.querySelector('.magic')
+// magicScore.addEventListener('click', () => {
+//     let magicAnswer = prompt('Guess the magic keyword. You win if answer is correct!')
+//     if (magicAnswer === 'fire') {
+//         round += 1
+//         myScore += 8
+//         myScoreDisplay.textContent = myScore
+//         isWinner()
+//     }
+// })
 
 
 /* pick rock paper or scissors randomly */
@@ -70,19 +73,26 @@ function computerPlay() {
     return rps[random]
 }
 
-
-
+let tryAgain = document.querySelector('.tryAgain')
+let endContainer = document.querySelector('.endContainer')
+let playContainer = document.querySelector('.playContainer')
+let winner = document.querySelector('.winner')
 function isWinner() {
-    if (round === winningScore) {
-        if (myScore > compScore) {
-            alert("You Won!")
-        } else if (myScore < compScore) {
-            alert("You Lost!")
-        } else {
-            alert("Draw! Same score")
-        }
+    playContainer.style.opacity = 0;
+    playContainer.style.transform = 'scale(0)';
+    playContainer.style.display = 'none';
+    endContainer.style.display = 'flex';
+    if (myScore > compScore) {
+        winner.textContent = "You Won!"
+    } else if (myScore < compScore) {
+        winner.textContent = "You Lost!"
+    } else {
+        winner.textContent = "Draw! Same score"
     }
 }
+
+
+
 
 /* plays a single round of Rock Paper Scissors 
 and declares the winner of the round */
